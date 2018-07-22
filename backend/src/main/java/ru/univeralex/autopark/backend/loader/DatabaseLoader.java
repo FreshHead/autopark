d09edc90d43fb711a553ecb2559fe26f293e5ce5
UniverAlex <UniverAlex@ya.ru>
2018-07-23 02:13:20 +0300

    if applied this commit will add more test wheeler data

--- backend/src/main/java/ru/univeralex/autopark/backend/loader/DatabaseLoader.java
+++ backend/src/main/java/ru/univeralex/autopark/backend/loader/DatabaseLoader.java
@@ -29,5 +29,12 @@ public class DatabaseLoader implements CommandLineRunner {
         this.carRepository.save(new Car("УАЗ-3962", "с051лк", 116, 2010, "Зарезервированно для Павлова Сергея"));
 
         this.wheelerRepository.save(new Wheeler("Павлов", "Сергей", "Константинович", 1994, "Сын Павлова К.И."));
+        this.wheelerRepository.save(new Wheeler("Мартынов", "Константин", "Васильевич", 1960, "Опытный водитель"));
+        this.wheelerRepository.save(new Wheeler("Петров", "Валентин", "Харитонович", 1973, ""));
+        this.wheelerRepository.save(new Wheeler("Иванов", "Александр", "Сергеевич", 1988, "Уволняется с 19.08.2018"));
+        this.wheelerRepository.save(new Wheeler("Сидоров", "Алексей", "Каримович", 1994, "Сын Павлова К.И."));
+        this.wheelerRepository.save(new Wheeler("Мартынов", "Константин", "Васильевич", 1952, "Делает сложный ремонт"));
+        this.wheelerRepository.save(new Wheeler("Валиуллин", "Марат", "Русланович", 1992, ""));
+        this.wheelerRepository.save(new Wheeler("Белкина", "Юлия", "Витальевна", 1989, "Имеет навыки фельдшера"));
     }
 }

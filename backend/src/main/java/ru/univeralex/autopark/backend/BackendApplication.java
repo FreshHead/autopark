1b2ba5335522cf871c2e9b8b0d1bc3939e1de699
UniverAlex <UniverAlex@ya.ru>
2018-07-22 19:09:51 +0300

    if applied this commit will fix CORS issue

--- backend/src/main/java/ru/univeralex/autopark/backend/BackendApplication.java
+++ backend/src/main/java/ru/univeralex/autopark/backend/BackendApplication.java
@@ -2,7 +2,11 @@ package ru.univeralex.autopark.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @RestController
@@ -10,5 +14,4 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
-
 }
